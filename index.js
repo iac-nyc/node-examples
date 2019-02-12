58b1b30ccd45312056b700bae00625db509c75e9
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-02-12 11:29:41 -0500

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var rect = require('./rectangle');
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with L = " + l + " and B = " + b);
-
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  L = "
-               + l + ",  and B = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
-}
-
+    console.log("Solving for rectangle with L = "
+                + l + " and B = " + b);
+    rect(l,b, (err,rectangle) => {// third parameter is a callback function
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions L = "
+                + l + " and B = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions L = "
+                + l + " and B = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");// This line will be printed before the setTimeout delay
+};
+console.log("----------\nL = Length, B = Breadth\n----------\n");
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,19 @@
-exports.perimeter =  (x, y) => (2*(x+y));
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() => 
+            // callback takes two value: error and return value  
+            // error       
+            callback(new Error("Rectangle dimensions should be greater than zero: L = "
+                + x + ", and B = " + y), 
+            null),
+            2000);
+    else
+        setTimeout(() => 
+            // this callback returing the value; not error that's why its null       
+            callback(null, {
+                perimeter: () => (2*(x+y)),
+                area:() => (x*y)
+            }), 
+            2000);
+}
 
-exports.area = (x, y) => (x*y);
